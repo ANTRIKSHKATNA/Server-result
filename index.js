f85a4175be6f75200c3971ca6887dcc2c33239d5
ANTRIKSHKATNA <antrikshkatna03@gmail.com>
2024-07-11 13:16:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const fs = require("fs");
+const Student = require("./model/studetModel");
+
+const server = express();
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/result")
+  .then(() => console.log("connected to db successfully"));
+
+let data = fs.readFileSync("./detailed_result.json", "utf-8");
+
+let parsedData = JSON.parse(data);
+
+async function abcd() {
+  let student = await Student.insertMany(parsedData);
+  console.log(student[0]);
+}
+
+server.use(express.json());
+
+server.get("/user", async (req, res) => {
+  let roll = req.headers.roll_number;
+  let data = await Student.findOne({ roll_number: roll });
+  res.send(data);
+});
+
+server.listen(8000, () => {
+  console.log("server is listening on port 8000");
+});
